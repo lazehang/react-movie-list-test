33e3667a0bc60a715fc6b4e07d11efbb3729f821
lazehang <lazehang@gmail.com>
2018-07-20 04:53:30 +0800

    back img fix

--- src/components/partials/Navbar.js
+++ src/components/partials/Navbar.js
@@ -13,7 +13,7 @@ export default class Navigation extends React.Component {
     render() {
         return (
             <Navbar className="pb-0" inverse fixed="top" expand="md" style={navStyle}>
-                <NavbarBrand href="/"><img src="/images/back.png" alt="back img" className="img img-responsive" height="20px"/> Romantic Comedy</NavbarBrand>
+                <NavbarBrand href="/"><img src="/images/Back.png" alt="back img" className="img img-responsive" height="20px"/> Romantic Comedy</NavbarBrand>
                 <img src="/images/search.png" height="20px" alt="search icon" />
             </Navbar>
         )
